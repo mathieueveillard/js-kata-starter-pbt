ec346ad34039234abf0c7a83b6a153e8661f8359
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2021-01-10 12:26:30 +0100

    With PBT

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# js-kata-starter
+# js-kata-starter-pbt
 
-Starter project for kata (Node, TypeScript, Jest)
+Starter project for kata with Property-Based Testing (Node, TypeScript, Jest, JSVerify)
 
 ## Installation
 
 ```text
-git clone https://github.com/mathieueveillard/js-kata-starter.git
-cd js-kata-starter
+git clone https://github.com/mathieueveillard/js-kata-starter-pbt.git
+cd js-kata-starter-pbt
 npm install
 ```
 
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,18 @@
-import { systemUnderTest } from ".";
+import * as jsc from "jsverify";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
+function linear(x: number): number {
+  return 0.5 * x + 1;
+}
+
+it("the slope between two points should be constant", function () {
+  const constantSlope = jsc.forall(jsc.integer, jsc.integer, function (x1: number, x2: number): boolean {
+    if (x1 === x2) {
+      return true;
+    }
+    const y1 = linear(x1);
+    const y2 = linear(x2);
+    const slope = (y2 - y1) / (x2 - x1);
+    return slope === 0.5;
   });
+  jsc.assert(constantSlope);
 });
