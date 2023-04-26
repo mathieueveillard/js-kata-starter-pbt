20e9da951a5e6f3fd91eadfef3e36310cecc397c
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2023-04-26 10:04:51 +0200

    Add logs

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,6 +8,7 @@ it("the slope between two points should be constant", function () {
       const y1 = linear(x1);
       const y2 = linear(x2);
       const slope = (y2 - y1) / (x2 - x1);
+      console.log({ x1, x2, y1, y2, slope });
       return slope === 0.5;
     }
   );
